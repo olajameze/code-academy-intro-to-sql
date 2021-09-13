f72e98f7819bf8239dd5e9ebdb75189277724933
James Gomez <olagomez@live.co.uk>
2021-09-13 12:56:57 +0000

    changes

--- Tables/code academy demo session.sql
+++ Tables/code academy demo session.sql
@@ -1,14 +1,17 @@
 create SCHEMA CodeAcademy;
 
+--@block
+
 SELECT *
 FROM CodeAcademy.customer
 LIMIT 100;
 
-SELECT customerName,
-customerName&Email,...
+--@block
+
+SELECT customer_name,
+customerName&Email,
 FROM table_name;
 ORDER BY column_1 ASC ,column_2 DESC;
-
 
 
 --@block
@@ -31,8 +34,41 @@ A.customer_name
 from CodeAcademy.customer A
 inner join CodeAcademy.order B
  on A.customer_id = B.customer_id
- limit 10;
+ limit 30;
 
 --@block
 
-select
+SELECT "order".order_id, customer.customer_name
+FROM CodeAcademy.customer
+INNER JOIN CodeAcademy."order" ON "order".customer_id
+ = customer.customer_id;
+
+--@block
+
+ SELECT 
+A.customer_name
+,B.total_amount
+from CodeAcademy.customer A
+LEFT join CodeAcademy.order B
+ on A.customer_id = B.customer_id
+ limit 100;
+
+ --@block
+
+ select customer_name
+ from CodeAcademy.customer
+ where phone_number = '';
+
+
+--@block
+
+SELECT customer_name, 
+aggregate_function('order') 
+FROM CodeAcademy 
+GROUP BY customer_name;
+
+--@block
+
+ select customer_name
+ from CodeAcademy.customer
+ where city = customer_name;
